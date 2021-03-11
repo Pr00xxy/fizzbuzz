966e19e60ddf3ef7b738b540355f014dc3c57b36
Pr00xxy <hampus.westman@gmail.com>
2021-03-11 15:59:20 +0100

    add base dataprovider

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -7,9 +7,53 @@ use PHPUnit\Framework\TestCase;
 class FizzBuzzTest extends TestCase
 {
 
-    protected function setUp(): void
+    public function numberDataProvider(): array
     {
-        new FizzBuzz();
+        return [
+            [
+                'numbers' => [
+                    1 => 1,
+                    2 => 2,
+                    3 => 'Fizz',
+                    4 => 4,
+                    5 => 'Buzz',
+                    6 => 'Fizz',
+                    7 => 7,
+                    8 => 8,
+                    9 => 'Fizz',
+                    10 => 'Buzz',
+                    11 => 11,
+                    12 => 'Fizz',
+                    13 => 13,
+                    14 => 14,
+                    15 => 'FizzBuzz',
+                    16 => 16,
+                    17 => 17,
+                    18 => 'Fizz',
+                    19 => 19,
+                    20 => 'Buzz',
+                    21 => 'Fizz',
+                    22 => 22,
+                    23 => 23,
+                    24 => 'Fizz',
+                    25 => 'Buzz',
+                    26 => 26,
+                    27 => 'Fizz',
+                    28 => 28,
+                    29 => 29,
+                    30 => 'FizzBuzz',
+                    31 => 31,
+                    32 => 32,
+                    33 => 'Fizz',
+                    34 => 34,
+                    35 => 'Buzz',
+                    36 => 'Fizz',
+                    37 => 37,
+                    38 => 38,
+                    39 => 'Fizz',
+                    40 => 'Buzz'
+                ]
+            ]
+        ];
     }
-
 }
